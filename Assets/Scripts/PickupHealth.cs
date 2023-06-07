9800a2be514695a774f623e165cb6701a493270a
SuicidalCarrot <jzvara0@yahoo.com>
2023-06-07 18:18:07 -0500

    fix maybe

--- Assets/Scripts/PickupHealth.cs
+++ Assets/Scripts/PickupHealth.cs
@@ -6,15 +6,11 @@ public class PickupHealth : Pickup
 {
     public byte health;
     protected override void onTouch(){
-        StartCoroutine(touch_CR());
-    }
-    private IEnumerator touch_CR(){
         if(play.getHealth() != play.maxHealth){
             aux.Play();
             Destroy(gameObject,1);
             bc.size = new Vector2(0.001f,0.001f);
             play.restoreHealth(health);
-            yield return new WaitForSeconds(0.25f);
             sr.enabled = false;
         }
     }
